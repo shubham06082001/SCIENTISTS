4ae0be5841082082c1eb252204828261b877152b
SHUBHAM KUMAR <shubhamkmr06082001@gmail.com>
2020-07-30 19:07:34 +0530

    Updates

---
+++ precache-manifest.17a7fc7b51d30f807b49a1e596a6a2cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f991cd24b26ae1144d188460d6a008",
+    "url": "/SCIENTISTS/index.html"
+  },
+  {
+    "revision": "8193444d3a41568c21af",
+    "url": "/SCIENTISTS/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "af317eb47f287409406e",
+    "url": "/SCIENTISTS/static/css/main.9eac878c.chunk.css"
+  },
+  {
+    "revision": "8193444d3a41568c21af",
+    "url": "/SCIENTISTS/static/js/2.bf97640c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SCIENTISTS/static/js/2.bf97640c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af317eb47f287409406e",
+    "url": "/SCIENTISTS/static/js/main.65af66e9.chunk.js"
+  },
+  {
+    "revision": "1dca6a873db0dc17b846",
+    "url": "/SCIENTISTS/static/js/runtime-main.56ee398d.js"
+  },
+  {
+    "revision": "bf7586e1171730ab5fbbc6f432109b7e",
+    "url": "/SCIENTISTS/static/media/Ellenoire.bf7586e1.woff"
+  }
+]);
